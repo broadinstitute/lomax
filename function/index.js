a442808ef63167692a688388b11ba119e48ccc66
David An <davidan@broadinstitute.org>
2019-02-05 16:15:12 -0500

    add todos to code:

--- function/index.js
+++ function/index.js
@@ -4,6 +4,21 @@ const Firestore = require('@google-cloud/firestore');
 
 const lomax = async (req, res) => {
 
+    // TODO: routing for the two types of HTTP requests: 1) create archive; 2) archive status
+    // TODO: (??) routing for pubsub trigger for syncing jobs
+
+    // create-archive path:
+    // TODO: validate non-empty Authorization header exists in request
+    // TODO: validate workspace, source bucket, destination bucket input params
+    // TODO: call to Sam to authn/authz user, via Authorization token
+    // TODO: read whitelist from bucket and validate user is whitelisted
+    // TODO: query Firestore for pre-existing jobs on this workspace
+    //     TODO: if any jobs exist, query to see if they are still active
+    //     TODO: update Firestore if any jobs have finished
+    // TODO: using Rawls bucket SA, add STS SA to source and destination buckets
+    // TODO: create STS job
+    // TODO: insert created STS job to firestore
+
     const firestore = new Firestore();
     let collectionRef = firestore.collection('archivejobs');
 
