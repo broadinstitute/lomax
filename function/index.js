1546ba2e0cea45904b5b4ced0e2957ab5ef83051
David An <davidan@broadinstitute.org>
2019-02-05 15:55:44 -0500

    skeleton connection to firestore

--- function/index.js
+++ function/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const lomax = function(req, res) {
-    res.status(418).json({hello:'lomax'});
+const Firestore = require('@google-cloud/firestore');
+
+const lomax = async (req, res) => {
+
+    const firestore = new Firestore();
+    let collectionRef = firestore.collection('archivejobs');
+
+    let docs = await collectionRef.listDocuments();
+    res.status(418).json(docs);
+
 };
 
 module.exports = {lomax};
